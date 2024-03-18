2ef230384fa3b305023005c429de2b6b4110f99b
Nafisa <Nafisafaruna@gmail.com>
2024-03-18 16:42:58 +0100

    first commit

---
+++ src/components/Heading.jsx
@@ -0,0 +1,14 @@
+import TagLine from "./Tagline";
+
+function Heading({ className, title, text, tag }) {
+  return (
+    <div className={`${className} max-w-[50rem] mx-auto mb-12 md:text-center lg:mb-20`}>
+      {title && <h2 className="h2">{title}</h2>}
+      {tag && (<TagLine className="mb-4 md:justify-center">{tag}</TagLine>)}
+      {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
+     
+    </div>
+  );
+}
+
+export default Heading;
